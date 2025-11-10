4c879966206fff5060bc9b91aa5bfb1ffd1c6462
Lucas Kronemberger <dev.kronemberger@gmail.com>
2025-11-10 15:23:18 -0300

    adicionado 3 perguntas no quiz

--- app/src/main/java/com/example/ap2_lucaskronemberger/QuizDigitalActivity.kt
+++ app/src/main/java/com/example/ap2_lucaskronemberger/QuizDigitalActivity.kt
@@ -1,5 +1,6 @@
 package com.example.ap2_lucaskronemberger
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.RadioGroup
@@ -15,6 +16,9 @@ class QuizDigitalActivity : AppCompatActivity() {
     private lateinit var tempoDeTela: RadioGroup
     private lateinit var notificacaoSilenciada: Switch
     private lateinit var btNext: Button
+    private lateinit var usoCelularManha: Switch
+    private lateinit var modoNoturno: Switch
+    private lateinit var checarMadruga: RadioGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +27,9 @@ class QuizDigitalActivity : AppCompatActivity() {
         tempoDeTela = findViewById(R.id.tempo_de_tela)
         notificacaoSilenciada = findViewById(R.id.notificacao_silenciada)
         btNext = findViewById(R.id.bt_next)
+        usoCelularManha = findViewById(R.id.uso_celular_acordar)
+        modoNoturno = findViewById(R.id.modo_noturno_luz_azul)
+        checarMadruga = findViewById(R.id.checar_madruga)
 
         btNext.setOnClickListener {
             collectDataAndProceed()
@@ -35,7 +42,7 @@ class QuizDigitalActivity : AppCompatActivity() {
 
         // pergunta 1
         when (tempoDeTela.checkedRadioButtonId) {
-            R.id.rb_tela1 -> score +- 10
+            R.id.rb_tela1 -> score += 10
             R.id.rb_tela2 -> score += 7
             R.id.rb_tela3 -> score += 3
             R.id.rb_tela4 -> score += 0
@@ -47,19 +54,38 @@ class QuizDigitalActivity : AppCompatActivity() {
 
         // pergunta 2
         if (isFormValid) {
-            if (notificacaoSilenciada.isChecked) {
-                score += 5
-            } else {
-                score += 0
-            }
+            if (notificacaoSilenciada.isChecked) score += 5
+        }
 
-            if (isFormValid) {
-                val intent = Intent(this, RotinaActivity::class.java)
-                intent.putExtra("PONTOS_DIGITAL", score)
-                startActivity(intent)
+        // pergunta 3
+        if (isFormValid) {
+            if (usoCelularManha.isChecked) score += 5
+        }
 
-                println("Pontuação Digital Calculada: $score")
+        //pergunta 4
+        if (isFormValid) {
+            if (modoNoturno.isChecked) score += 5
+        }
+
+        //pergunta 5
+        if (isFormValid) {
+            when (checarMadruga.checkedRadioButtonId) {
+                R.id.madruga_1 -> score += 10
+                R.id.madruga_2 -> score += 5
+                R.id.madruga_3 -> score += 0
+                else -> {
+                    isFormValid = false
+                    Toast.makeText(this, "Por favor, responda a pergunta 5", Toast.LENGTH_SHORT).show()
+                }
             }
+        }
+
+        if (isFormValid) {
+            val intent = Intent(this, RotinaActivity::class.java)
+            intent.putExtra("PONTOS_DIGITAL", score)
+            startActivity(intent)
+
+            println("Pontuação Digital Total Calculada: $score")
         }
     }
 }
